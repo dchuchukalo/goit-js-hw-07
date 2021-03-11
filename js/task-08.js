856020c2de977c37192461144097a894aa28e9fd
Dmytro Chuchukalo <52856976+dchuchukalo@users.noreply.github.com>
2021-03-11 22:13:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,6 +2,7 @@ const inputRef = document.querySelector('div#controls input');
 const render = document.querySelector('[data-action=render]');
 const destroy = document.querySelector('[data-action=destroy]');
 const boxesRef = document.querySelector('div#boxes');
+const arrOfDiv = [];
 
 render.addEventListener('click', () => createBoxes());
 
@@ -11,9 +12,9 @@ const createBoxes = amount => {
   amount = Number(inputRef.value);
   const basicSize = 30;
   const randomColor = () => Math.floor(Math.random() * 256);
-  const arrOfDiv = [];
+  const total = amount + arrOfDiv.length
 
-  for (let i = arrOfDiv.lenght; i < amount + arrOfDiv.lenght; i += 1) {
+  for (let i = arrOfDiv.length; i < total; i += 1) {
     const div = document.createElement('div');
     let size = basicSize + i * 10;
     div.style = `width: ${size}px; height: ${size}px; background-color: rgba( ${randomColor()} , ${randomColor()} , ${randomColor()} )`;
